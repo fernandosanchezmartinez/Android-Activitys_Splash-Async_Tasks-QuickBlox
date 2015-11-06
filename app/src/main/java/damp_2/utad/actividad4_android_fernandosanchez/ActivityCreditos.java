d8db6b14bda077e380abdafd0ce96442834b9ef8
fernando.sanchez <fsm_031@hotmail.com>
2015-11-06 12:16:03 +0100

    Se crea el metodo cargar tabla en el qbactivity creamos la clase task admin con los metodos start y establecer listener correspondiente. en el metodo run, se implementa la ejecucion y ciclos. Se implementan los metodos de la activityCreditos y en el task admin listener.

---
+++ app/src/main/java/damp_2/utad/actividad4_android_fernandosanchez/ActivityCreditos.java
@@ -0,0 +1,18 @@
+package damp_2.utad.actividad4_android_fernandosanchez;
+
+import damp_2.utad.milibreria.TaskAdmin.TaskAdmin.TaskAdminListener;
+
+/**
+ * Created by fernando.sanchez on 06/11/2015.
+ */
+public class ActivityCreditos implements TaskAdminListener{
+    @Override
+    public void taskCycleFinished(int iNumOfCycles) {
+
+    }
+
+    @Override
+    public void taskCycleFinishedAll() {
+
+    }
+}
