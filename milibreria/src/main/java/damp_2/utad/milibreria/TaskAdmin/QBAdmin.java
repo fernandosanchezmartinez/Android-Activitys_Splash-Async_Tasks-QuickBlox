2de7d5d79b186ca008e893df11e1484b3d1490c3
fernando.sanchez <fsm_031@hotmail.com>
2015-11-06 11:32:35 +0100

    se terminan de exportar los jars. en el android manifest se le añeade la linea para dar permisos de internet a la app.  En el mainactivity se crea un objeto qbadmin se le añade el listener correspondiente y se inicia la session.  Todos éstos métodos se han creado en sus correspondientes clases.  Se crea la activity Creditos que se usara posteriormente. en la clase qb admin se gestiona el inicio de sesión basico y conexión con QuickBlox.

--- milibreria/src/main/java/damp_2/utad/milibreria/TaskAdmin/QBAdmin.java
+++ milibreria/src/main/java/damp_2/utad/milibreria/TaskAdmin/QBAdmin.java
@@ -1,7 +1,56 @@
 package damp_2.utad.milibreria.TaskAdmin;
 
+import android.content.Context;
+import android.os.Bundle;
+
+import com.quickblox.auth.QBAuth;
+import com.quickblox.auth.model.QBSession;
+import com.quickblox.core.QBEntityCallbackImpl;
+import com.quickblox.core.QBSettings;
+import com.quickblox.users.model.QBUser;
+
+import java.util.List;
+
 /**
  * Created by fernando.sanchez on 06/11/2015.
  */
 public class QBAdmin {
+
+    private QBAdminListener listener;
+
+    private QBUser qbUser = null;
+    private Context context;
+
+
+   // private QBAdminListener listener;
+
+
+
+    public void addQbAdminListener(QBAdminListener listener){
+        this.listener=listener;
+    }
+
+    public QBAdmin(Context context) {
+         this.context =context;
+
+        QBSettings.getInstance().fastConfigInit("30679", "UBpAyQm-F6t4ER5", "BxSGKUDdzpeXWgd");
+    }
+
+    public void simpleSession(){
+    QBAuth.createSession(new QBEntityCallbackImpl<QBSession>() {
+        @Override
+        public void onSuccess(QBSession session, Bundle params) {
+            boolean sesionCreada = true;
+            listener.simpleSessionCreated(sesionCreada);
+            // success
+        }
+
+        @Override
+        public void onError(List<String> errors) {
+            boolean sesionCreada = false;
+            listener.simpleSessionCreated(sesionCreada);
+            // errors
+        }
+    });
+    }
 }
