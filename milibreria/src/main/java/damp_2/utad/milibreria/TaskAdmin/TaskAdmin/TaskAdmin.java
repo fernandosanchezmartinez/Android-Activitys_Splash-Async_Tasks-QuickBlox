5c2a95364e5424b020657e0a05b84962336ed2ad
fernando.sanchez <fsm_031@hotmail.com>
2015-11-13 12:39:08 +0100

    Se implementa la calse Activity creditos. se declaran 2 variables y una variable estado, en el run se hacen las comparaciones y se cambian las vistas o los activitys en funcion de donde se encuentre en la ejecucion del programa. En la activiticreditos mediante el método runOnThread ejecutamos ese run compartido en los dos metodos de los ciclos.  Por otro lado se modifica el método run del TaskAdmin.

--- milibreria/src/main/java/damp_2/utad/milibreria/TaskAdmin/TaskAdmin/TaskAdmin.java
+++ milibreria/src/main/java/damp_2/utad/milibreria/TaskAdmin/TaskAdmin/TaskAdmin.java
@@ -1,7 +1,46 @@
 package damp_2.utad.milibreria.TaskAdmin.TaskAdmin;
 
+import android.content.Intent;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by fernando.sanchez on 06/11/2015.
  */
-public class TaskAdmin {
+public class TaskAdmin extends TimerTask {
+    private TaskAdminListener listener;
+    private static long SPLASH_SCREEN_DELAY = 3000;// 10 seg
+    private int iNumOfCycles=2;// un ciclo por imagen
+    private Timer timer = null;
+    //private TimerTask task = new TimerTask();
+
+    public TaskAdmin(long delay, int numOfCycles){
+        SPLASH_SCREEN_DELAY=delay;
+        iNumOfCycles=numOfCycles;
+        timer = new Timer();
 }
+
+    public void addQTaskAdminListener(TaskAdminListener listener){
+        this.listener=listener;
+    }
+
+    public void startTasks(){
+        timer.schedule(this, SPLASH_SCREEN_DELAY,SPLASH_SCREEN_DELAY);
+    }
+    public void setTasksAdminListener(TaskAdminListener listener){
+        this.listener=listener;
+    }
+
+    @Override
+    public void run() {
+
+        iNumOfCycles--;
+
+        //if(listener!=null)listener.taskCicloAcabado(iNumOfCycles);
+
+        if(iNumOfCycles==0){
+            timer.cancel();
+            if(listener!=null)listener.taskTodosCiclosAcabados();
+        }else if(listener!=null)listener.taskCicloAcabado(iNumOfCycles);
+}}
