41b88b3f86b4d3a8cecf5e0bf3921bbee08fd25e
fernando.sanchez <fsm_031@hotmail.com>
2015-11-13 11:47:49 +0100

    Se trabaja con la clase Activity Creditos. Se añaden la imagenes al proyecto y se inicializan en ésta clase(se buscan por id). En los métodos taskCicloAcabado se produce el cambio de imagen cada vez que finaliza un ciclo, ésto se hace cambiando la visibilidad de las 2 imagenes declaradas.  En el método taskTodosCiclosAcabados se inicia el main activity que contendrá los datos de la tabal de Quikblox.

--- app/src/main/java/damp_2/utad/actividad4_android_fernandosanchez/ActivityCreditos.java
+++ app/src/main/java/damp_2/utad/actividad4_android_fernandosanchez/ActivityCreditos.java
@@ -1,18 +1,50 @@
 package damp_2.utad.actividad4_android_fernandosanchez;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import damp_2.utad.milibreria.TaskAdmin.TaskAdmin.TaskAdmin;
 import damp_2.utad.milibreria.TaskAdmin.TaskAdmin.TaskAdminListener;
 
 /**
  * Created by fernando.sanchez on 06/11/2015.
  */
-public class ActivityCreditos implements TaskAdminListener{
+public class ActivityCreditos extends AppCompatActivity implements TaskAdminListener{
+
+    TaskAdmin taskAdmin;
+
+    ImageView imagen1 = null;
+    ImageView imagen2 = null ;
+
+
     @Override
-    public void taskCycleFinished(int iNumOfCycles) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditos);
+
+        imagen1 = (ImageView) findViewById(R.id.androidlogo1);
+        imagen2 = (ImageView) findViewById(R.id.cargando);
+
+        taskAdmin = new TaskAdmin(10000, 1);
+        taskAdmin.addQTaskAdminListener(this);
 
     }
 
+
     @Override
-    public void taskCycleFinishedAll() {
+    public void taskCicloAcabado(int iNumOfCycles) {
+        imagen1.setVisibility(View.GONE);
+        imagen2.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void taskTodosCiclosAcabados() {
+        Intent i = new Intent(this, MainActivity.class );
+        startActivity(i);
 
     }
 }
